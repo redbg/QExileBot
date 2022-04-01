b875f73c965c2a208eda922d39cd5a70b9062fb4
redbg <705642344@qq.com>
2022-04-01 17:02:12 +0800

    fix bug

--- Account.h
+++ Account.h
@@ -55,17 +55,26 @@ protected:
         m_ExileGame->m_ExileClient = m_ExileClient;
 
         // errorOccurred
-        connect(m_ExileClient, &ExileClient::errorOccurred, this, [=](QAbstractSocket::SocketError socketError)
-                { m_BackendError = m_ExileClient->m_BackendError; this->quit(); });
-        connect(m_ExileGame, &ExileGame::errorOccurred, this, [=](QAbstractSocket::SocketError socketError)
-                { m_BackendError = m_ExileClient->m_BackendError; this->quit(); });
+        connect(
+            m_ExileClient, &ExileClient::errorOccurred, this, [=](QAbstractSocket::SocketError socketError)
+            { m_BackendError = m_ExileClient->m_BackendError; this->quit(); },
+            Qt::DirectConnection);
+
+        connect(
+            m_ExileGame, &ExileGame::errorOccurred, this, [=](QAbstractSocket::SocketError socketError)
+            { m_BackendError = m_ExileClient->m_BackendError; this->quit(); },
+            Qt::DirectConnection);
 
         // 登录成功
-        connect(m_ExileClient, &ExileClient::LoginSuccess, this, [=](const QString &AccountName)
-                { m_AccountName = AccountName; });
+        connect(
+            m_ExileClient, &ExileClient::LoginSuccess, this, [=](const QString &AccountName)
+            { m_AccountName = AccountName; },
+            Qt::DirectConnection);
 
         // 选择角色成功,进入游戏
-        connect(m_ExileClient, &ExileClient::SelectCharacterSuccess, m_ExileGame, &ExileGame::connectToGameServer);
+        connect(m_ExileClient, &ExileClient::SelectCharacterSuccess,
+                m_ExileGame, &ExileGame::connectToGameServer,
+                Qt::DirectConnection);
 
         // 连接登录服务器
         m_ExileClient->connectToLoginServer(Global::hostName, Global::port, m_Email, m_Password);
